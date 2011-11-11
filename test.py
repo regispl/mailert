4a02d40a75b06d77e45cb920103c9ab208abe978
Michał M <github@michalski.im>
2011-11-11 20:18:04 +0100

    cleaning repo

--- test.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/python2.7
-# -*- coding: utf8 -*-
-
-from mailert.mailert import Mailert
-
-m = Mailert(host='mail.triforce.pl', port=587, user='outgoing@triforce.pl', password='out321', service='Test Service', receivers=['m.michalski@money.pl'])
-
-m.info('Yebuo siem!', 'Detale wiadomości')
